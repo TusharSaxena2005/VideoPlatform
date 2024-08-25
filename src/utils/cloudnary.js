079b0ad12015178c6289bc7334e3571fc65354bc
Tushar Saxena <tushar.dec6@gmail.com>
2024-08-25 15:19:35 +0530

    setup cloudnary and multer

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary"
+import { response } from "express";
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
+
+const cloudnaryUpload = async (localPath) => {
+    try {
+        if (!localPath) return
+        cloudinary.uploader.upload(
+            localPath,
+            {
+                resource_type: "auto",
+            }
+        )
+        console.log("File has been uploaded on Cloudnary successfully", response.url);
+        return response;
+    } catch (error) {
+        fs.unlink(localPath);
+        return null;
+    }
+}
